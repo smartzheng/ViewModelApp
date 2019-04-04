4031b911d4170b8e3924f917c71c0bf7b1a35ded
shulin <shulin@uoko.com>
2019-04-04 10:09:48 +0800

    no message

--- app/src/main/java/com/smartzheng/viewmodelapp/ui/MainActivity.kt
+++ app/src/main/java/com/smartzheng/viewmodelapp/ui/MainActivity.kt
@@ -3,20 +3,27 @@ package com.smartzheng.viewmodelapp.ui
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.smartzheng.viewmodelapp.R
 import com.smartzheng.viewmodelapp.databinding.ActivityMainBinding
 import com.smartzheng.viewmodelapp.model.entity.Movie
 import com.smartzheng.viewmodelapp.ui.base.BaseActivity
 import com.smartzheng.viewmodelapp.viewmodel.MovieViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<ActivityMainBinding, MovieViewModel>() {
+    val adapter: MovieAdapter by lazy {
+        MovieAdapter()
+    }
 
     override fun initView() {
         viewModel = ViewModelProviders.of(this, ViewModelProvider.NewInstanceFactory()).get(MovieViewModel::class.java)
-        viewDataBinding.model = viewModel
-        viewModel.response.observe(this, Observer<Movie> {
+        viewDataBinding.viewModel = viewModel
+        rv.adapter = adapter
+        rv.layoutManager = LinearLayoutManager(this)
+        viewModel.getMovies().observe(this, Observer<Movie> {
             if (it != null) {
-                viewModel.adapter.addData(it.subjects)
+                adapter.addData(it.subjects)
             }
         })
     }
--- app/src/main/java/com/smartzheng/viewmodelapp/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/smartzheng/viewmodelapp/viewmodel/MovieViewModel.kt
@@ -1,22 +1,30 @@
 package com.smartzheng.viewmodelapp.viewmodel
 
 import android.text.Editable
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.smartzheng.viewmodelapp.model.api.ApiRequest
-import com.smartzheng.viewmodelapp.ui.MovieAdapter
+import com.smartzheng.viewmodelapp.model.entity.Movie
 
 /**
  * Created by smartzheng
  * 2019/3/28
  */
 class MovieViewModel : ViewModel() {
-    val adapter: MovieAdapter by lazy {
-        MovieAdapter()
-    }
+
     var title = MutableLiveData<String>()
-    var response = ApiRequest.get().top250()
+
+    private lateinit var movies: LiveData<Movie>
+
     fun afterTextChanged(s: Editable) {
         title.value = s.toString()
+    }
+
+    fun getMovies(): LiveData<Movie> {
+        if (!::movies.isInitialized) {
+            movies = ApiRequest.get().top250()
+        }
+        return movies
     }
 }
